86c33f908fb419dc8f876e987ce4eafeb47ad4aa
Keval <keval.multidots@gmail.com>
2017-03-29 17:34:06 +0530

    Initial commit

---
+++ app/src/main/java/ringotonepicker/sample/com/ringtonepicker/MainActivity.java
@@ -0,0 +1,13 @@
+package ringotonepicker.sample.com.ringtonepicker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
